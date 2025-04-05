8ac222db6808cab78af5b040d899ee7aacf3e742
unknown <asadshahi804@gmail.com>
2025-04-05 21:11:50 +0430

    create note button function

--- src/components/NotesDashboard.tsx
+++ src/components/NotesDashboard.tsx
@@ -44,12 +44,12 @@ export default function NotesDashboard() {
         <h1 className="text-3xl font-bold">My Notes</h1>
         <button
           onClick={handleAddNote}
-          className="flex items-center justify-center px-4 py-2 bg-primary-600 hover:bg-primary-700 text-white rounded-lg shadow transition-colors"
+          className="flex items-center  cursor-pointer justify-center px-4 py-2 bg-primary-600 hover:bg-primary-700  rounded-lg shadow transition-colors"
         >
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
           </svg>
-          New Note
+          Create New Note
         </button>
       </div>
 
