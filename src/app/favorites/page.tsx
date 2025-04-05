04407914a5a3430512cefdc6be5ee0054cb46c2e
unknown <asadshahi804@gmail.com>
2025-04-05 20:17:30 +0430

    favorite page ui

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,7 +1,11 @@
+import FavoritesPage from '@/components/FavoritesPage'
+import Layout from '@/components/Layout'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <Layout>
+      <FavoritesPage />
+    </Layout>
   )
 }
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,8 +1,11 @@
+import Layout from '@/components/Layout'
 import SettingsPage from '@/components/SettingsPage'
 import React from 'react'
 
 export default function page() {
   return (
-    <SettingsPage />
+    <Layout>
+      <SettingsPage />
+    </Layout>
   )
 }
