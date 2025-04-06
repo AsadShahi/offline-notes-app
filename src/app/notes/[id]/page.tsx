a0787de5cfc45b99d908114effdd9bd7327572be
unknown <asadshahi804@gmail.com>
2025-04-06 15:18:12 +0430

    use param fixed in building

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -3,13 +3,12 @@ import { use } from 'react'; // Required for unwrapping params
 import NoteEditor from '@/components/templates/NoteEditor';
 import Layout from '@/components/templates/Layout';
 
-// Next.js 15.2 expects params to be a Promise
 interface PageProps {
   params: Promise<{ id: string }>;
 }
 
 export default function NotePage({ params }: PageProps) {
-  // Unwrap the Promise with React's `use()`
+
   const { id } = use(params);
 
   return (
