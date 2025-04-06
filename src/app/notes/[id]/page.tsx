b7b13b9e60aaead5046260664407aa2addd8840b
unknown <asadshahi804@gmail.com>
2025-04-06 10:02:16 +0430

    params issu fixed in ssr

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,21 +1,17 @@
-/* Offline Notes App - Full Implementation with Next.js + TypeScript + Tailwind + Zustand using Tiptap */
 
-// src/components/editor.tsx
-"use client";
-import { EditorContent, useEditor } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import { useEffect } from "react";
 import NoteEditor from '@/components/NoteEditor'
 import Layout from '@/components/Layout'
 
-export default function NotePage({ params }: { params: { id: string } }) {
+interface PageProps {
+  params: {
+    id: string  
+  }
+}
+
+export default function NotePage({ params }: PageProps) {
   return (
     <Layout>
       <NoteEditor noteId={params.id} />
     </Layout>
   )
 }
-
-
-
-
